cf9fc94ad3b66c85d0fb69b730868d4da570fabe
John Chan <johnchan@eventxtra.com>
2019-11-28 14:51:29 +0800

    🎉 first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+import { NativeModules } from 'react-native';
+
+const { BrotherPrinter } = NativeModules;
+
+export default BrotherPrinter;
---
+++ ios/BrotherPrinter.h
@@ -0,0 +1,5 @@
+#import <React/RCTBridgeModule.h>
+
+@interface BrotherPrinter : NSObject <RCTBridgeModule>
+
+@end
